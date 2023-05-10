dbaaf78c59b3d3d82891dc1b838a80ffa9cb2a62
Tidjani Belmansour <tbelmansour@cofomo.com>
2023-05-09 21:37:48 -0400

    Add project files.

---
+++ H23_Dev_Info_TP3/DataConverters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H23_Dev_Info_TP3
+{
+    public class DataConverters
+    {
+		public static double FahrenheitEnCelsius(double temperatureF)
+		{
+			// la température maximale enregistrée sur Terre est de 134 degrés Fahrenheit
+			if(temperatureF > 134)
+				throw new ArgumentOutOfRangeException("La température ne peut pas être supérieure à 134 degrés Fahrenheit");
+			
+			// la température minimale enregistrée sur Terre est de -128 degrés Fahrenheit
+			if(temperatureF < -128)
+				throw new ArgumentOutOfRangeException("La température ne peut pas être inférieure à -128 degrés Fahrenheit");
+
+
+			double temperatureC = (temperatureF - 32) * 5 / 9;
+			return temperatureC;
+		}
+	}
+}
---
+++ H23_Dev_Info_TP3/DataValidators.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H23_Dev_Info_TP3
+{
+    public class DataValidators
+    {
+        public bool IsValidEmail(string email)
+        {
+            // la chaine de caractère ne doit pas être nulle ou vide
+            if(string.IsNullOrWhiteSpace(email)) 
+                return false;
+
+            // le courriel doit contenir un seul @
+            if (email.IndexOf("@") != email.LastIndexOf("@"))
+                return false;
+
+            // le courriel doit contenir un point
+            if (!email.Contains("."))
+                return false;
+
+            // email doit contenir au moins un point après le @
+            if (email.IndexOf("@") > email.LastIndexOf("."))
+                return false;
+
+            // email doit contenir au moins deux caractères après le dernier point
+            if (email.Length - email.LastIndexOf(".") < 3)
+                return false;
+
+            // email ne doit pas contenir d'espace
+            if (email.Contains(" "))
+                return false;
+
+            // email ne doit pas commencer ni se terminer par un point ou un @
+            if (email.StartsWith(".") || email.StartsWith("@") || email.EndsWith(".") || email.EndsWith("@"))
+                return false;
+
+            // email ne doit pas contenir de caractères
+            if (email.Contains("é") || email.Contains("è") || email.Contains("à") || email.Contains("ù") || email.Contains("ç"))
+                return false;
+
+            // email doit avoir au moins un caractère avant le @
+            if (email.IndexOf("@") < 1)
+                return false;
+
+            // sinon, on considère que le courriel est valide
+            return true;
+        }
+
+    }
+}
